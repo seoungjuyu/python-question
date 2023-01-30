f849bb49e583b8ef278ab6096971afa89f9f3344
seung ju <110443976+seoungjuyu@users.noreply.github.com>
2023-01-30 23:10:19 +0900

    python-question 1~10

---
+++ python1~10.py
@@ -0,0 +1,29 @@
+#문제1번 화면에 Hello World 문자열을 출력하세요.
+print("Hello World")
+
+#문제2번 화면에 Mary's cosmetics을 출력하세요.
+print("Mary's cosmetics")
+
+#문제3번 화면에 아래 문장을 출력하세요. 
+print('신씨가 소리질렀다. "도둑이야".')
+
+#문제4번 화면에 "C:\Windows"를 출력하세요.
+print("C:\Windows")
+
+#문제5번 다음 코드를 실행해보고 \t와 \n의 역할을 설명해보세요.
+print("안녕하세요. \n만나서\t\t반갑습니다.")  #'\t'는 탭을 의미하고 '\n'은 줄바꿈의 의미함
+
+#문제6번 print 함수에 두 개의 단어를 입력한 예제입니다. 아래 코드의 출력 결과를 예상해봅시다.
+print("오늘은", "일요일") #여러값을 출력하려면 쉼표로 구분
+
+#문제7번 print() 함수를 사용하여 다음과 같이 출력하세요.
+print("naver", "kakao", "samsung", sep=";")
+
+#문제8번 print() 함수를 사용하여 다음과 같이 출력하세요.
+print("naver", "kakao", "samsung", sep="/")
+
+#문제9번 다음 코드를 수정하여 줄바꿈이 없이 출력하세요.
+print("first", end="");print("second")
+
+#문제10번 5/3의 결과를 화면에 출력하세요.
+print(5/3)
