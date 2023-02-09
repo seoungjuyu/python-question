2fee5d9b73f5797aaeac0e30b83a5089a110ddf4
seungjuyu <2seungju0@gmail.com>
2023-02-09 23:33:22 +0900

    python-qustion #17

--- python11~20.py
+++ python11~20.py
@@ -9,13 +9,16 @@ print(s+"!",t)
 
 #14번
 print(2+2*3)
+
 #15번
 a = 123
 print (type(a))
 #16번
 print(type(int("720")))
 #17번
-
+num = 100
+result = str(num)
+print(result, type(result))
 #18번
 
 #19번
