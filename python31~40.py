e71a583f6efd79693a2289029de42482be7e8301
seungjuyu <2seungju0@gmail.com>
2023-03-05 23:22:38 +0900

    python #35 % formatting

--- python31~40.py
+++ python31~40.py
@@ -22,3 +22,13 @@ t2 = 'java'
 p = t1 + ' ' + t2 + ' '
 
 print(p * 4) #python java python java python java python java
+
+# 35
+
+name1 = "김민수" 
+age1 = 10
+name2 = "이철희"
+age2 = 13
+
+print("이름: %s 나이: %d" % (name1, age1))
+print("이름: %s 나이: %d" % (name2, age2))
