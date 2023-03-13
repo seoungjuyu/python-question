037ea5e068f78f15bef4c434cd8f9e4570c02a5d
seungjuyu <2seungju0@gmail.com>
2023-03-13 22:26:40 +0900

    python endswith

--- python41~50.py
+++ python41~50.py
@@ -13,3 +13,12 @@ text = 'hello'
 text1 = text.capitalize() #capitalize를 사용해 앞'h'를 대문자'H'로 번경
 print(text1)
 
+#44
+file_name = "보고서.xlsx"
+name = file_name.endswith("xlsx") #문자가 맞게끝나는지 확인
+print(name)
+
+#45
+file_name = "보고서.xlsx"
+name1 = file_name.endswith("xls,xlsx") #튜플을 적고 여려패턴을 적어주면 됨
+print(name1)
