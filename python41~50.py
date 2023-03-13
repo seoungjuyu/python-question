c000bf9c78d804fdb68dd963d56fb5ce438dc830
seungjuyu <2seungju0@gmail.com>
2023-03-13 22:13:20 +0900

    python capitalize

--- python41~50.py
+++ python41~50.py
@@ -5,5 +5,11 @@ print(Upper)
 
 #42
 ticker = "BTC_KRW"
-ticker1 = ticker.lower()
+ticker1 = ticker.lower() #lower를 사용해서 소문자로 변경
 print(ticker1)
+
+#43
+text = 'hello'
+text1 = text.capitalize() #capitalize를 사용해 앞'h'를 대문자'H'로 번경
+print(text1)
+
