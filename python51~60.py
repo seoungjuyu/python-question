1029285975b1f2e32716b8744f9b6cc5f0107353
seung <2seungju0@gmail.com>
2023-03-22 20:48:58 +0900

    python/insert

--- python51~60.py
+++ python51~60.py
@@ -5,3 +5,8 @@ movie = ["닥터 스레인지","스플릿","럭키"]
 movie = ["닥터 스레인지","스플릿","럭키"]
 movie.append("배트맨")
 print(movie)
+
+#53
+movie = ['닥터 스트레인지', '스플릿', '럭키', '배트맨']
+movie.insert(1,"슈퍼맨")   #list의 'inset(인덱스, 원소)' 특정위치게 값을 끼워넣을수 있음
+print(movie)
