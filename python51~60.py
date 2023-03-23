3a021acb822ec88ef9f1bf9463a99356d8f98240
seung <2seungju0@gmail.com>
2023-03-23 23:14:12 +0900

    python/list_del

--- python51~60.py
+++ python51~60.py
@@ -10,3 +10,8 @@ print(movie)
 movie = ['닥터 스트레인지', '스플릿', '럭키', '배트맨']
 movie.insert(1,"슈퍼맨")   #list의 'inset(인덱스, 원소)' 특정위치게 값을 끼워넣을수 있음
 print(movie)
+
+#54
+movie = ['닥터 스트레인지', '슈퍼맨', '스플릿', '럭키', '배트맨']
+del movie[3]
+print(movie)
