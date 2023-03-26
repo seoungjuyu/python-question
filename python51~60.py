18dcc90d3fb59f0f80f00c1382067f7232d882d4
seung <2seungju0@gmail.com>
2023-03-26 21:17:09 +0900

    python/list del

--- python51~60.py
+++ python51~60.py
@@ -15,3 +15,10 @@ print(movie)
 movie = ['닥터 스트레인지', '슈퍼맨', '스플릿', '럭키', '배트맨']
 del movie[3]
 print(movie)
+
+#55
+# del을 이용해서 원소를 삭제할수는 있지만 리스트에서 어떤 값을 삭제하면 남은 값들을 새로 인덱싱이 됨.
+movie = ['닥터 스트레인지', '슈퍼맨', '스플릿', '배트맨']
+del movie[2]
+del movie[2]  
+print(movie)
