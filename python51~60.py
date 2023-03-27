a53b872e73d1174c7623615c292278374ac85f32
seung <2seungju0@gmail.com>
2023-03-27 22:37:29 +0900

    python list/len

--- python51~60.py
+++ python51~60.py
@@ -43,3 +43,7 @@ print("min: ", min(nums))
 nums = [1, 2, 3, 4, 5]
 sum_nums = sum(nums)
 print(sum_nums)
+
+#59
+cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "소시지", "라면", "팥빙수", "김치전"]
+print(len(cook))
