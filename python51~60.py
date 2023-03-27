1a6abfe42c0894288a28bf795d6551c82b6d039a
seung <2seungju0@gmail.com>
2023-03-27 22:41:16 +0900

    python list/평균구하기(sum/len)

--- python51~60.py
+++ python51~60.py
@@ -47,3 +47,8 @@ print(sum_nums)
 #59
 cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "소시지", "라면", "팥빙수", "김치전"]
 print(len(cook))
+
+#60
+nums = [1, 2, 3, 4, 5]
+r_nums = sum(nums)/len(nums)
+print(r_nums)
