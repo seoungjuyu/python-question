62301edd6ae96fe139e4c78d7ad2eb09642f2c9f
seung <2seungju0@gmail.com>
2023-04-01 19:23:54 +0900

    python/f-string

--- python31~40.py
+++ python31~40.py
@@ -42,6 +42,19 @@ age2 = 13
 print("이름 : {}, 나이 : {}".format(name1, age1))
 print("이름 : {} 나이 : {}".format(name2, age2))
 # 37
+name1 = "김민수" 
+age1 = 10
+name2 = "이철희"
+age2 = 13
+
+print(f'이름 : {name1}, 나이: {age1}')
+print(f'이름 : {name2}, 나이: {age2}')
+
+
+
+# f-string -> 문자열 앞에 f가 붙은 형태,
+# f-string을 사용하면 '{변수}'와 같은 형태로 문자열 사이에 타입과 상관없이 값을 출력가능
+
 
 # 38 
 상장주식수 = "5,969,782,550"
