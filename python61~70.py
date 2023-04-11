b7502bff5e29a6dbc57bde636fe52452da1808bc
seung <2seungju0@gmail.com>
2023-04-11 21:26:04 +0900

    python|join

--- python61~70.py
+++ python61~70.py
@@ -25,3 +25,7 @@ print(" ".join(interest))
 #67
 interest = ['삼성전자', 'LG전자', 'Naver', 'SK하이닉스', '미래에셋대우']
 print("/".join(interest))
+
+#68
+interest = ['삼성전자', 'LG전자', 'Naver', 'SK하이닉스', '미래에셋대우']
+print("/n".join(interest))
