5e87e4598b5e47431c5644b9e9e0c0e6771a78d8
seung <2seungju0@gmail.com>
2023-04-12 23:17:00 +0900

    python|split

--- python61~70.py
+++ python61~70.py
@@ -29,3 +29,8 @@ print("/".join(interest))
 #68
 interest = ['삼성전자', 'LG전자', 'Naver', 'SK하이닉스', '미래에셋대우']
 print("/n".join(interest))
+
+#69
+string = "삼성전자/LG전자/Naver"
+interest = string.split("/")
+print(interest)
