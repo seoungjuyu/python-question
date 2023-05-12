fc9a0e53d869d18114ad9653814ba290f8aa61cf
seung <2seungju0@gmail.com>
2023-05-12 13:17:21 +0900

    python#74 tuple

--- python71~80.py
+++ python71~80.py
@@ -10,3 +10,13 @@ movie_rank = ('닥터 스트레인지','스플릿', '럭키')
 #73
 number = (0,1,2,3,4,)
 print(number[1])
+
+#74
+# >> t = (1, 2, 3)
+# >> t[0] = 'a'
+# Traceback (most recent call last):
+#   File "<pyshell#46>", line 1, in <module>
+#     t[0] = 'a'
+# TypeError: 'tuple' object does not support item assignment
+
+# -> tuple은 원소의 값을 변경할 수 없음
